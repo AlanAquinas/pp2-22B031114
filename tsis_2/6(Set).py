f72982b2977f521629a5451646663cd65b8c963f
Arslan <116744376+AlanAquinas@users.noreply.github.com>
2023-02-04 17:26:34 +0600

    Add files via upload

---
+++ tsis_2/6(Set).py
@@ -0,0 +1,20 @@
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+
+thisset = {"apple", "banana", "cherry", "apple"}
+print(thisset)
+
+
+thisset = {"apple", "banana", "cherry"}
+print(len(thisset))
+
+set1 = {"abc", 34, True, 40, "male"} # A set can contain different data types:
+
+
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
+
+
+thisset = set(("apple", "banana", "cherry")) # note the double round-brackets
+print(thisset)
